8ddf86dade175b64c01acf29ac4ed7068e792cc1
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2020-09-01 16:51:05 +0530

    Add files via upload

---
+++ php-projects/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>code wire .co</title>
+    <link type="image/png" rel="icon" href="./img/title.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/bootstrap-grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque:400,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- header nav bar -->
+    <header id="home">
+        <nav>
+          <ul>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#services">SERVICES</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="">CONTACT</a></li>
+          </ul>
+        </nav>
+        <!-- hero page -->
+        <div class="divider">
+            <h2>GLORIOUS<br>HEROIC<br>MAGNIFIC<br>ROYAL</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+              officia.</p>
+            <a Action=""class="button" href="signup.php">Register</a>
+        </div>
+      </header>
+
+
+
+
+
+      <!-- services -->
+        <div id="services">
+          <h1>SERVICES</h1>
+          <!-- first section -->
+          <section class="project blue">
+            <div class="container">
+             <div class="row">
+                  <div class="col-sm">
+                  <h2 class="royal">Services</h2>
+                  <p class="prata">Lorem,
+                    ipsum dolor sit amet consectetur adipisicing elit. Illo ea consectetur necessitatibus magnam in tempore.
+                    Earum quis dolor nam nihil debitis possimus ipsam quod laudantium impedit,
+                    adipisci dolorum architecto atque.</p>
+                    <a href="" type="button">get here</a>
+                  </div>
+                  <div class="col-sm">
+                    <img src="./img/da.png"
+                    alt="data analysis">
+                  </div>
+             </div>
+            </div>
+          </section>
+           
+            <!-- second section -->
+              <section>
+                <div class="container">
+                  <div class="row">  
+                    <div class="col-sm">
+                       <img src="./img/de.png" alt="design">
+                    </div>
+                    <div class="col-sm">
+                        <h2 class="royal">About</h2>
+                        <p class="prata">Lorem,
+                          ipsum dolor sit amet consectetur adipisicing elit. Illo ea consectetur necessitatibus magnam in
+                          tempore. Earum quis dolor nam nihil debitis possimus ipsam quod laudantium impedit,
+                          adipisci dolorum architecto atque.</p>
+                          <a href="" type="button">get here</a>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <!-- third section -->
+                <section class="project blue">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm">
+                        <h2 class="royal">Services</h2>
+                        <p class="prata">Lorem,
+                          ipsum dolor sit amet consectetur adipisicing elit. Illo ea consectetur necessitatibus magnam in
+                          tempore. Earum quis dolor nam nihil debitis possimus ipsam quod laudantium impedit,
+                          adipisci dolorum architecto atque.</p>
+                          <a href="" type="button">get here</a>
+                      </div>
+                      <div class="col-sm">
+                          <img src="./img/fi.png"
+                          alt="finance">
+                      </div>
+                    </div>
+                  </div>
+                </section>
+        </div>
+
+
+
+
+        <!-- about -->
+        <div>
+          <h1 class="head">ABOUT</h1>
+            <figure class="about flex" id="about">
+                <div>
+                  <img src="./img/about.png" alt="">
+                </div>
+                <div class="align">
+                  <h2>lorem40</h2>
+                  <p>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem, ipsum.</li>
+                    <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem.</li>
+                  </p>
+                </div>
+            </figure>
+        </div>
+
+
+
+
+          <!-- testimonials -->
+            <div></div>
+
+
+
+
+
+
+            <!-- footer -->
+        <h1>CONTACT</h1>
+        <footer>
+                      <div class="container">
+                  <div class="social-icon-container">
+                <a href="#"><img src="./img/facebook.svg" class="social-icon"></a>
+                <a href="#"><img src="./img/instagram.svg" class="social-icon"></a>
+                <a href="#"><img src="./img/pinterest.svg" class="social-icon"></a>
+                <a href=""><img src="./img/twitter.svg" class="social-icon"></a>
+                <p>designed with love y abhisek from riseu.com<br> www.riseu.com @rayabhisek....</p>
+              </div>
+            </div>
+        </footer>
+  </body>
+
+  </html>
